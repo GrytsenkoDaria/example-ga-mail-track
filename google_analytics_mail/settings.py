812e9ebe2ccfde7f340b037e9ec399e664f7d1bb
GrytsenkoDaria <grytsenkodaria@gmail.com>
2022-05-21 17:54:43 +0300

    locals

--- google_analytics_mail/settings.py
+++ google_analytics_mail/settings.py
@@ -2,8 +2,6 @@ import os
 import django_heroku
 
 
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -118,3 +116,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
